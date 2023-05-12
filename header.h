abfb6a29bce7ba8a175e31dc498fd2e2b9898644
ItsAras <48598204+ItsAras@users.noreply.github.com>
2023-05-12 14:47:16 +0300

    Update header.h

--- header.h
+++ header.h
@@ -1,69 +1,38 @@
-#include <iostream>
-#include <fstream>
-#include <iomanip>
-#include <algorithm>
-#include <vector>
-#include <string>
-#include <chrono>
-#include <iterator>
+#include "header.h"
 
-using std::cout;
-using std::cin;
-using std::endl;
-using std::string;
-using std::setw;
-using std::copy;
-using std::left;
-using std::right;
-using std::setprecision;
-using std::fixed;
-using std::sort;
-using std::vector;
-using std::ifstream;
-using std::ofstream;
-using std::getline;
-using std::to_string;
-using std::find_if;
-
-class Student
+int main()
 {
-    private:
-        string name, surname;
-        int exam;
-        double average;
-        double median;
+    char userInput;
+    vector<Student> student;
+    vector<Student> noob;
+    vector<Student> nerd;
+    Student tmp;
+    srand(time(NULL));
 
-    public:
-        vector<int> grades;
-        Student() : exam(0) { }
-        Student(std::istream& is);
+    cout << "Do you want to read data from a file? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') readFile(student, tmp, noob, nerd);
 
-        inline string getName() const { return name; };
-        inline string getSurname() const { return surname; };
-        const vector<int>& getGrades() const { return grades; };
-        inline int getExam() const { return exam; };
-        inline double getAverage() const { return average; };
-        inline double getMedian() const { return median; };
+    cout << "Do you want to generate a file? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') generateFile(student, tmp, noob);
 
-        void setName(string name) { this->name = name; };
-        void setSurname(string surname) { this->surname = name; };
-        void setGrades(vector<int> grades) { this->grades = grades; }
-        void setExam(int exam) { this->exam = exam; }
-        void setAverage(double average) { this->average = average; }
-        void setMedian(double median) { this->median = median; }
-        void clearGrades() { grades.clear(); }
-        void sortGrades() { sort(grades.begin(), grades.end()); };
-};
+    cout << "Do you want to run a benchmark? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') sortingTest(student, tmp, noob, nerd);
 
-void getGrades(Student &);
-void findAverage(Student &);
-void findMedian(Student &);
-void getStudent(Student &);
-void printStudents(vector<Student>);
-void printStudentsToFile(vector<Student>);
-void readFile(vector<Student> &, Student &, vector<Student>, vector<Student>);
-void generateFile(vector<Student> &, Student &, vector<Student>);
-void sortingTest(vector<Student> &, Student &, vector<Student>, vector<Student>);
-int getRandomGrade();
-void sortStudentsMethodOne(vector<Student>, vector<Student> &);
-void sortStudentsMethodTwo(vector<Student>, vector<Student> &, vector<Student> &);
+    else
+    {
+        do
+        {
+            getStudent(tmp);
+            student.push_back(tmp);
+
+            cout << "Press N when finished. Press any key to continue... ";
+            cin >> userInput;
+
+        } while (userInput != 'n'  || userInput == 'N');
+
+        printStudents(student);
+    }
+}
