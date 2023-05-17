c41d31d0ee6527e404945c0fe4f6819900fc4dbc
ItsAras <48598204+ItsAras@users.noreply.github.com>
2023-05-17 08:14:21 +0300

    Add files via upload

---
+++ header.h
@@ -0,0 +1,98 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <iterator>
+#include "gtest/gtest.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+using std::setw;
+using std::copy;
+using std::left;
+using std::right;
+using std::setprecision;
+using std::fixed;
+using std::sort;
+using std::vector;
+using std::ifstream;
+using std::ofstream;
+using std::getline;
+using std::to_string;
+using std::find_if;
+
+
+
+class Human
+{
+    protected:
+        string name;
+        string surname;
+
+    public:
+        Human() = default;
+        Human(const string& name, const string& surname)
+            : name(name), surname(surname) {}
+
+        virtual ~Human() = default;
+
+        inline string getName() const { return name; }
+        inline string getSurname() const { return surname; }
+
+        void setName(const string& name) { this->name = name; }
+        void setSurname(const string& surname) { this->surname = surname; }
+};
+
+class Student : public Human {
+private:
+    int exam;
+    double average;
+    double median;
+    
+
+public:
+    vector<int> grades;
+
+    Student() : exam(0), average(0), median(0) {}
+    Student(const string& name, const string& surname)
+        : Human(name, surname), exam(0), average(0), median(0) {}
+    Student(const string& name, const string& surname, const vector<int>& grades, int exam, double average, double median)
+        : Human(name, surname), exam(exam), average(average), median(median), grades(grades) {}
+
+    const vector<int>& getGrades() const { return grades; }
+    inline int getExam() const { return exam; }
+    inline double getAverage() const { return average; }
+    inline double getMedian() const { return median; }
+
+    void setGrades(const vector<int>& grades) { this->grades = grades; }
+    void setExam(int exam) { this->exam = exam; }
+    void setAverage(double average) { this->average = average; }
+    void setMedian(double median) { this->median = median; }
+    void clearGrades() { grades.clear(); }
+    void sortGrades() { sort(grades.begin(), grades.end()); }
+
+    virtual ~Student() = default;
+    Student(const Student& other) = default;
+    Student& operator=(const Student& other) = default;
+    Student(Student&& other) noexcept = default;
+    Student& operator=(Student&& other) noexcept = default;
+};
+
+void getGrades(Student &);
+void findAverage(Student &);
+void findMedian(Student &);
+void getStudent(Student &);
+void printStudents(vector<Student>);
+void printStudentsToFile(vector<Student>);
+void readFile(vector<Student> &, Student &, vector<Student>, vector<Student>);
+void generateFile(vector<Student> &, Student &, vector<Student>);
+void sortingTest(vector<Student> &, Student &, vector<Student>, vector<Student>);
+int getRandomGrade();
+void sortStudentsMethodOne(vector<Student>, vector<Student> &);
+void sortStudentsMethodTwo(vector<Student>, vector<Student> &, vector<Student> &);
+void runTest();
---
+++ main.cpp
@@ -0,0 +1,44 @@
+#include "header.h"
+
+int main(int argc, char* argv[])
+{
+    char userInput;
+    vector<Student> student;
+    vector<Student> noob;
+    vector<Student> nerd;
+    Student tmp;
+    srand(time(NULL));
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+
+    cout << "Do you want to read data from a file? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') readFile(student, tmp, noob, nerd);
+
+    cout << "Do you want to generate a file? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') generateFile(student, tmp, noob);
+
+    cout << "Do you want to run a benchmark? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') sortingTest(student, tmp, noob, nerd);
+
+    cout << "Do you want to run a test for (Rule Of Five)? Y - (YES), N - (NO): ";
+    cin >> userInput;
+    if (userInput == 'y' || userInput == 'Y') runTest();
+
+    else
+    {
+        do
+        {
+            getStudent(tmp);
+            student.push_back(tmp);
+
+            cout << "Press N when finished. Press any key to continue... ";
+            cin >> userInput;
+
+        } while (userInput != 'n'  || userInput == 'N');
+
+        printStudents(student);
+    }
+}
