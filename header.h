ba9912585b9198ee5148c5e85cc383c4653cce68
ItsAras <48598204+ItsAras@users.noreply.github.com>
2023-05-18 21:35:07 +0300

    Update header.h

--- header.h
+++ header.h
@@ -6,7 +6,6 @@
 #include <string>
 #include <chrono>
 #include <iterator>
-#include "gtest/gtest.h"
 
 using std::cout;
 using std::cin;
@@ -26,61 +25,97 @@ using std::getline;
 using std::to_string;
 using std::find_if;
 
-
-
 class Human
 {
     protected:
         string name;
         string surname;
 
+
     public:
         Human() = default;
         Human(const string& name, const string& surname)
             : name(name), surname(surname) {}
-
         virtual ~Human() = default;
-
-        inline string getName() const { return name; }
-        inline string getSurname() const { return surname; }
-
-        void setName(const string& name) { this->name = name; }
-        void setSurname(const string& surname) { this->surname = surname; }
+        virtual inline string getName() const = 0;
+        virtual inline string getSurname() const = 0;
+        virtual void setName(const string& name) = 0;
+        virtual void setSurname(const string& surname) = 0;
 };
 
 class Student : public Human {
-private:
-    int exam;
-    double average;
-    double median;
-    
+    private:
+        int exam;
+        double average;
+        double median;
+        
+    public:
+        vector<int> grades;
 
-public:
-    vector<int> grades;
+        Student() : exam(0), average(0), median(0) {}
+        Student(const string& name, const string& surname)
+            : Human(name, surname), exam(0), average(0), median(0) {}
+        Student(const string& name, const string& surname, const vector<int>& grades, int exam, double average, double median)
+            : Human(name, surname), exam(exam), average(average), median(median), grades(grades) {}
 
-    Student() : exam(0), average(0), median(0) {}
-    Student(const string& name, const string& surname)
-        : Human(name, surname), exam(0), average(0), median(0) {}
-    Student(const string& name, const string& surname, const vector<int>& grades, int exam, double average, double median)
-        : Human(name, surname), exam(exam), average(average), median(median), grades(grades) {}
+        inline string getName() const { return name; }
+        inline string getSurname() const { return surname; }
+        const vector<int>& getGrades() const { return grades; }
+        inline int getExam() const { return exam; }
+        inline double getAverage() const { return average; }
+        inline double getMedian() const { return median; }
 
-    const vector<int>& getGrades() const { return grades; }
-    inline int getExam() const { return exam; }
-    inline double getAverage() const { return average; }
-    inline double getMedian() const { return median; }
 
-    void setGrades(const vector<int>& grades) { this->grades = grades; }
-    void setExam(int exam) { this->exam = exam; }
-    void setAverage(double average) { this->average = average; }
-    void setMedian(double median) { this->median = median; }
-    void clearGrades() { grades.clear(); }
-    void sortGrades() { sort(grades.begin(), grades.end()); }
+        void setName(const string& name) { this->name = name; }
+        void setSurname(const string& surname) { this->surname = surname; }
+        void setGrades(const vector<int>& grades) { this->grades = grades; }
+        void setExam(int exam) { this->exam = exam; }
+        void setAverage(double average) { this->average = average; }
+        void setMedian(double median) { this->median = median; }
+        void clearGrades() { grades.clear(); }
+        void sortGrades() { sort(grades.begin(), grades.end()); }
 
-    virtual ~Student() = default;
-    Student(const Student& other) = default;
-    Student& operator=(const Student& other) = default;
-    Student(Student&& other) noexcept = default;
-    Student& operator=(Student&& other) noexcept = default;
+        Student& operator=(Student&& other) noexcept {
+            if (this != &other) {
+                Human::operator=(std::move(other));
+                exam = other.exam;
+                average = other.average;
+                median = other.median;
+                grades = std::move(other.grades);
+
+                other.name = "";
+                other.surname = "";
+                other.exam = 0;
+                other.average = 0;
+                other.median = 0;
+                other.grades.clear();
+            }
+            return *this;
+        }
+
+        Student(Student&& other) noexcept
+            : Human(std::move(other)), exam(other.exam), average(other.average), median(other.median), grades(std::move(other.grades)) {
+            other.name = "";
+            other.surname = "";
+            other.exam = 0;
+            other.average = 0;
+            other.median = 0;
+            other.grades.clear();
+        }
+
+        Student& operator=(const Student& other) {
+            if (this != &other) {
+                Human::operator=(other);
+                exam = other.exam;
+                average = other.average;
+                median = other.median;
+                grades = other.grades;
+            }
+            return *this;
+        }
+
+        Student(const Student& other)
+        : Human(other), exam(other.exam), average(other.average), median(other.median), grades(other.grades) {}
 };
 
 void getGrades(Student &);
